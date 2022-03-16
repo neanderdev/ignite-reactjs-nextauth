b9421e9e0ba0220196bcfaaf386ee8f87dd73b4a
neanderdev <neander.faria@hotmail.com>
2022-03-16 08:33:52 -0300

    Validating authentication (Server)

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,8 +1,9 @@
-import { useEffect } from "react";
-import { useContext } from "react";
+import { useEffect, useContext } from "react";
 
 import { AuthContext } from "../contexts/AuthContext";
-import { api } from "../services/api";
+import { setupAPICliente } from "../services/api";
+import { api } from '../services/apiClient';
+import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
     const { user } = useContext(AuthContext);
@@ -20,3 +21,14 @@ export default function Dashboard() {
         </div>
     );
 }
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+    const apiClient = setupAPICliente(ctx);
+    const response = await apiClient.get('/me');
+
+    console.log(response.data);
+
+    return {
+        props: {}
+    };
+})
